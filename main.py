c2001124182272c84c912b334f3425e1fd3bb789
Mohd Sabir <sabir9202@gmail.com>
2022-03-30 23:48:40 +0530

    CORS Bug Fix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware;
 app = FastAPI()
 origins = [
     "http://localhost:3000",
-    "https://plag-catcher.vercel.app/",
+    "https://plag-catcher.vercel.app",
 ]
 
 app.add_middleware(
